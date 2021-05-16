5201dedb8c262da85cbcfead313fc144f2b7b94e
yoonsangkwak <yunsang08@naver.com>
2021-05-16 12:53:39 +0900

    splash

---
+++ FindKindergarten/app/src/main/java/site/yoonsang/findkindergarten/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package site.yoonsang.findkindergarten.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import site.yoonsang.findkindergarten.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ FindKindergarten/app/src/main/java/site/yoonsang/findkindergarten/view/SplashActivity.kt
@@ -0,0 +1,19 @@
+package site.yoonsang.findkindergarten.view
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import site.yoonsang.findkindergarten.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+        }, 1500)
+    }
+}
